3d8585c36110e1cd3ddb72b779da53b43ca6dd59
ilakumar <ilakkumar@gmail.com>
2017-07-14 14:16:34 -0400

    retrieve question from database

--- router/api/training.js
+++ router/api/training.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var session = require('express-session');
-var flash = require('express-flash');
-var config = require('../../config.js');
-var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var Question = require('../../models/Question');
 
 module.exports = function(router){
   router.post('/training/question', function(req, res){
-    MongoClient.connect(config.database, function(err, db) {
-      if (err) throw err;
-      var stats = db.collection("questions").stats();
-      console.log(stats);
-      console.log("finished retrieving question data from test database");
-      res.json({
-        msg: "finished getting question"
+    var db = mongoose.createConnection('mongodb://localhost/test');
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+      Question.findOne({'id':'1'}, function (err, question) {
+        if (err) {
+          res.json({
+            err: err
+          });
+        } else {
+          console.log('question data: ', question);
+          res.json({
+            msg: "finished getting question data"
+          });
+        }
       });
     });
   });
