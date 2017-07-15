271435e1f75e5b52e54cf464d557068c9e16ba86
ilakumar <ilakkumar@gmail.com>
2017-07-15 00:19:08 -0400

    sent question info from database to web

--- router/api/training.js
+++ router/api/training.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
-var Question = require('../../models/Question');
 
 module.exports = function(router){
   router.post('/training/question', function(req, res){
     var db = mongoose.createConnection('mongodb://localhost/test');
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
-      Question.findOne({'id':'1'}, function (err, question) {
+      var collection = db.collection('questions');
+      collection.findOne({'id':'1'}, function (err, question) {
         if (err) {
           res.json({
             err: err
           });
         } else {
-          console.log('question data: ', question);
           res.json({
+            question : question,
             msg: "finished getting question data"
           });
         }
