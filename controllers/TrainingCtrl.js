da98d2ab532d8ae76576ed2db194eb9b085a9f72
ilakumar <ilakkumar@gmail.com>
2017-07-16 18:45:27 -0400

    get list of questions

---
+++ controllers/TrainingCtrl.js
@@ -0,0 +1,54 @@
+var async = require('async');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+module.exports = {
+  getQuestions: function(options, callback){
+
+    async.waterfall([
+
+      function(done){
+        var random = Math.floor(Math.random() * 6);
+        var randomIds = [random];
+        while (randomIds.length < 5) {
+          var isRepeat = true;
+          while (isRepeat) {
+            random = Math.floor(Math.random() * 6);
+            for (var i = 0; i < randomIds.length; i++) {
+              if (randomIds[i] == random) {
+                break;
+              }
+              else if (i == randomIds.length - 1) {
+                isRepeat = false;
+                randomIds[randomIds.length] = random;
+              }
+            }
+          }
+        }
+        done(null, randomIds);
+      },
+
+      function(randomIds, done){
+        var db = mongoose.createConnection('mongodb://localhost/test');
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function() {
+          var collection = db.collection('questions');
+          var questions = [];
+          randomIds.forEach(function(item, index) {
+            collection.findOne({'id':item.toString()}, function (err, question) {
+              if (err) {
+                return done(err);
+              }
+              else {
+                questions[index] = question;
+                if (questions.length == 5) {
+                  done(null, questions);
+                }
+              }
+            });
+          });
+        });
+      },
+    ], callback);
+  },
+};
--- router/api/training.js
+++ router/api/training.js
@@ -1,59 +1,16 @@
-var mongoose = require('mongoose');
+var TrainingCtrl = require('../../controllers/TrainingCtrl');
 
 module.exports = function(router){
-  router.post('/training/question', function(req, res){
-    var db = mongoose.createConnection('mongodb://localhost/test');
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-      var collection = db.collection('questions');
-      var questions = [];
-      var random = Math.floor(Math.random() * 6);
-      var randomIds = [random];
-      while(randomIds.length < 5) {
-        var isRepeat = true;
-        while (isRepeat) {
-          random = Math.floor(Math.random() * 6);
-          for (var i = 0; i < randomIds.length; i++) {
-            if (randomIds[i] == random) {
-              break;
-            }
-            else if (i == randomIds.length - 1) {
-              isRepeat = false;
-              randomIds[randomIds.length] = random;
-            }
-          }
-        }
-      }
-      console.log(randomIds);
-      for (var i = 0; i < randomIds.length; i++) {
-        collection.findOne({'id':randomIds[i].toString()}, function (err, question) {
-          if (err) {
-            res.json({
-              err: err
-            });
-          }
-          else {
-            questions[i] = question;
-            console.log(questions);
-          }
+	router.post('/training/questions', function(req, res){
+		TrainingCtrl.getQuestions({}, function(err, questions){
+			if (err){
+				res.json({err: err});
+			} else {
+				res.json({
+          msg: 'Questions retrieved from database',
+          questions: questions
         });
-      }
-      res.json({
-        questions : questions,
-        msg: "finished getting question data"
-      });
-      // collection.findOne({'id':'1'}, function (err, question) {
-      //   if (err) {
-      //     res.json({
-      //       err: err
-      //     });
-      //   } else {
-      //     res.json({
-      //       question : question,
-      //       msg: "finished getting question data"
-      //     });
-      //   }
-      // });
-    });
-  });
+			}
+		});
+	});
 };
