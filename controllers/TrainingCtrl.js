d785aa2277b956f7c7d27ea6ce3b9ec3aeb68a7c
ilakumar <ilakkumar@gmail.com>
2017-07-19 16:33:51 -0400

    remove waterfall from ctrl

--- controllers/TrainingCtrl.js
+++ controllers/TrainingCtrl.js
@@ -7,51 +7,42 @@ module.exports = {
   getQuestions: function(options, callback){
     var quizType = JSON.parse(JSON.stringify(options.quizType));
 
-    async.waterfall([
-
-      function(done){
-        var random = Math.floor(Math.random() * 6);
-        var randomIds = [random];
-        while (randomIds.length < 5) {
-          var isRepeat = true;
-          while (isRepeat) {
-            random = Math.floor(Math.random() * 6);
-            for (var i = 0; i < randomIds.length; i++) {
-              if (randomIds[i] == random) {
-                break;
-              }
-              else if (i == randomIds.length - 1) {
-                isRepeat = false;
-                randomIds[randomIds.length] = random;
-              }
-            }
+    var random = Math.floor(Math.random() * 6);
+    var randomIds = [random];
+    while (randomIds.length < 5) {
+      var isRepeat = true;
+      while (isRepeat) {
+        random = Math.floor(Math.random() * 6);
+        for (var i = 0; i < randomIds.length; i++) {
+          if (randomIds[i] == random) {
+            break;
+          }
+          else if (i == randomIds.length - 1) {
+            isRepeat = false;
+            randomIds[randomIds.length] = random;
           }
         }
-        console.log(randomIds);
-        done(null, randomIds);
-      },
+      }
+    }
 
-      function(randomIds, done){
-        var db = mongoose.createConnection(config.database);
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.once('open', function(){
-          var collection = db.collection(quizType);
-          var questions = [];
-          randomIds.forEach(function(item, index) {
-            collection.findOne({'id':item.toString()}, function (err, question) {
-              if (err) {
-                return done(err);
-              }
-              else {
-                questions[index] = question;
-                if (questions.length == 5) {
-                  done(null, questions);
-                }
-              }
-            });
-          });
+    var db = mongoose.createConnection(config.database);
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function(){
+      var collection = db.collection(quizType);
+      var questions = [];
+      randomIds.forEach(function(item, index) {
+        collection.findOne({'id':item.toString()}, function (err, question) {
+          if (err) {
+            return callback(err);
+          }
+          else {
+            questions[index] = question;
+            if (questions.length == 5) {
+              return callback(null, questions);
+            }
+          }
         });
-      },
-    ], callback);
+      });
+    });
   },
 };
