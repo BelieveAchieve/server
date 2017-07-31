80a9456e57d8b5602e462c677fd98ac0545d1823
ilakumar <ilakkumar@gmail.com>
2017-07-31 14:28:09 -0400

    working on adding images

--- models/Question.js
+++ models/Question.js
@@ -6,6 +6,7 @@ var questionSchema = new mongoose.Schema({
   correctAnswer: String,
   category: String,
   subcategory: String,
+  image: String
 });
 
 // Given a question record, strip out sensitive data for public consumption
@@ -13,7 +14,8 @@ questionSchema.methods.parseQuestion = function(){
   return {
     _id: this._id,
     questionText: this.questionText,
-    possibleAnswers: this.possibleAnswers
+    possibleAnswers: this.possibleAnswers,
+    image: this.image
   };
 };
 
