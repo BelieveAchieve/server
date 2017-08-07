214b72ef47405f11bfa8e7071593eb02dc2ed9e2
ilakumar <ilakkumar@gmail.com>
2017-08-07 12:16:08 -0400

    updated re corey's suggestions

--- controllers/TrainingCtrl.js
+++ controllers/TrainingCtrl.js
@@ -3,7 +3,8 @@ var User = require('../models/User');
 var ObjectId = require('mongodb').ObjectID;
 
 // Fisher-Yates shuffle
-function shuffle(array) {
+function shuffle(origArray) {
+  var array = origArray.slice(0); // clones the array
   var currIndex = array.length,
       tempValue,
       randomIndex;
@@ -25,42 +26,34 @@ function shuffle(array) {
 
 module.exports = {
   getQuestions: function(options, callback){
-    var category = JSON.parse(JSON.stringify(options.category));
-    var subcategories = Question.getSubcategories(category);
+    var subcategories = Question.getSubcategories(options.category);
 
-    // create an array of arrays of questions (divided by subcategory)
-    var questionsBySubcategory = [];
-    subcategories.map(function(subcategory) {
-      questionsBySubcategory[subcategory] = [];
-    });
-
-    Question.find({ 'category': category }, function(err, questions) {
+    Question.find({ 'category': options.category }, function(err, questions) {
       if (err){
         return callback(err);
       }
       else {
         var randomQuestions = [];
-
-        // sort questions by subcategory into arrays
-        questions.map(function(question) {
+        var questionsBySubcategory = questions.reduce(function(acc, question) {
           var subcategory = question.subcategory;
-          questionsBySubcategory[subcategory].push(question);
-        });
+          if (acc[subcategory] == null){
+            // If null, subcategory has not been initialized, so create an array with the question in it:
+            acc[subcategory] = [question]
+            return acc;
+          } else {
+            // Since the key is not null, we can assume we have previously populated it with an array holding a question
+            acc[subcategory].push(question);
+            return acc;
+          }
+        }, {});
 
         // get x unique, random objects from n objects in arrays
         subcategories.map(function(subcategory) {
           var questions = questionsBySubcategory[subcategory];
-          // initialize array of values from 0 to questions.length - 1
-          var array = [];
-          for (var i = 0; i < questions.length; i++) {
-            array[i] = i;
-          }
-          array = shuffle(array);
-
+          questions = shuffle(questions);
           // change depending on how many of each subcategory are wanted
           for (var i = 0; i < 1; i++) {
-            var index = array[i];
-            var question = questions[index];
+            var question = questions[i];
             randomQuestions.push(question);
           }
         });
@@ -82,7 +75,7 @@ module.exports = {
         return callback(err);
       }
       else {
-        questions.map(function(question) {
+        questions.forEach(function(question) {
           var correctAnswer = question.correctAnswer;
           var userAnswer = idAnswerMap[question._id];
           if (correctAnswer == userAnswer) {
@@ -91,10 +84,10 @@ module.exports = {
         });
         User.findOne({'_id': userid}, function(err, user){
           if (err){
-            return done(err);
+            return callback(err);
           }
           if (!user) {
-            return done(new Error('No account with that id found.'));
+            return callback(new Error('No account with that id found.'));
           }
           user.score = score;
           user.save(function(err, user){
