4d4c5ab6049efb7677427d172cc05c328a409d10
zuoyuanh <david@collegemate.me>
2019-02-22 15:15:07 -0800

    implemented time zone feature for calendar

--- router/api/calendar.js
+++ router/api/calendar.js
@@ -34,5 +34,28 @@ module.exports = function(router){
         });
 			}
 		});
+	});
+	router.post('/calendar/tz/get', function(req, res){
+    CalendarCtrl.getTimezone({userid: req.body.userid}, function(err, tz){
+			if (err){
+				res.json({err: err});
+			} else {
+				res.json({
+          msg: 'Timezone retrieved',
+          tz: tz
+        });
+			}
+		});
+  });
+	router.post('/calendar/tz/save', function(req, res){
+    CalendarCtrl.updateTimezone({userid: req.body.userid, tz: req.body.tz}, function(err, tz){
+      if (err) {
+				res.json({err: err});
+			} else {
+				res.json({
+          msg: 'Timezone saved'
+        });
+			}
+		});
   });
 };
