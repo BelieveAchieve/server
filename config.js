67a8b4feba61292286150ca3b4c3153ab6e1fa28
Jake Romer <mail@jakeromer.com>
2019-03-08 20:00:17 -0500

    Move configuration to environment variables

---
+++ config.js
@@ -0,0 +1,21 @@
+// Server configuration
+
+module.exports = {
+  environment: process.env.UPCHIEVE_SERVER_ENV,
+  serverHost: process.env.UPCHIEVE_SERVER_HOST,
+  serverPort: process.env.UPCHIEVE_SERVER_PORT,
+  clientHost: process.env.UPCHIEVE_CLIENT_HOST,
+  clientPort: process.env.UPCHIEVE_CLIENT_PORT,
+  dbHost: process.env.UPCHIEVE_SERVER_DB_HOST,
+  sessionSecret: process.env.UPCHIEVE_SERVER_SESSION_SECRET,
+  socketsPort: process.env.UPCHIEVE_SERVER_SOCKETS_PORT,
+  saltRounds: 10,
+  sendgridApiKey: process.env.UPCHIEVE_SENDGRID_API_KEY,
+  sendgridTemplateId: process.env.UPCHIEVE_SENDGRID_TEMPLATE_ID,
+  upchieveNoreplySender: process.env.UPCHIEVE_NOREPLY_SENDER,
+  twilioAccountSid: process.env.UPCHIEVE_TWILIO_ACCOUNT_SID,
+  twilioAuthToken: process.env.UPCHIEVE_TWILIO_AUTH_TOKEN,
+  twilioSendingNumber: process.env.UPCHIEVE_TWILIO_SENDING_NUMBER,
+  cleanSpeakApiKey: process.env.UPCHIEVE_CLEANSPEAK_API_KEY,
+  isProd: () => this.environment === 'production'
+}
