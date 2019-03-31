8fc19a868b1edb47666962582f2b57ae4d742915
Jake Romer <mail@jakeromer.com>
2019-03-31 17:50:38 -0400

    Add update logic

--- controllers/QuestionCtrl.js
+++ controllers/QuestionCtrl.js
@@ -1,43 +1,20 @@
 const Question = require('../models/Question')
 
-const list = (options, cb) => {
-  return new Promise((resolve, reject) => {
-    Question.find({}, (err, question) => {
-      if (err || !question) {
-        reject(new Error('could not get questions'))
-      } else {
-        resolve(question)
-      }
-    })
-  })
+const list = async (options, cb) => {
+  return Question.find({})
 }
 
-const get = (options, cb) => {
-  const { questionId } = options
+const update = async options => {
+  const { id, question } = options
 
-  Question.findById(questionId, (err, question) => {
-    if (err || !question) {
-      cb(new Error('could not get question'))
-    } else {
-      cb(question)
-    }
-  })
-}
-
-const update = (options, cb) => {
-  const { questionId, question } = options
-
-  Question.update({ _id: questionId }, question, (err, question) => {
-    if (err || !question) {
-      cb(new Error('could not update question'))
-    } else {
-      cb(question)
-    }
-  })
+  return Question.findOneAndUpdate(
+    { _id: id },
+    { $set: question },
+    { new: true, upsert: true }
+  )
 }
 
 module.exports = {
   list: list,
-  get: get,
   update: update
 }
--- router/admin/questions.js
+++ router/admin/questions.js
@@ -5,17 +5,37 @@ const ejs = require('ejs')
 const QuestionCtrl = require('../../controllers/QuestionCtrl')
 
 module.exports = router => {
-  router.route('/questions').get((req, res) => {
-    QuestionCtrl.list().then(questions => {
-      const templateName = 'questions'
-      const templatesDir = path.join(path.resolve(__dirname), 'templates')
-      const templateFile = path.join(templatesDir, `${templateName}.ejs`)
+  router.route('/questions').get(async (req, res) => {
+    const templateName = 'questions'
+    const templatesDir = path.join(path.resolve(__dirname), 'templates')
+    const templateFile = path.join(templatesDir, `${templateName}.ejs`)
+    let questions = []
+
+    try {
+      questions = await QuestionCtrl.list()
+
       ejs.renderFile(templateFile, { questions }, null, (err, str) => {
         if (err) {
           res.send('404 Not Found')
         }
+
         res.send(str)
       })
-    })
+    } catch (_error) {
+      res.send('500 Internal Server Error')
+    }
+  })
+
+  router.route('/question/:id').put(async (req, res) => {
+    try {
+      const updatedQuestion = await QuestionCtrl.update({
+        id: req.params.id,
+        question: req.body.question
+      })
+
+      res.status(200).json({ question: updatedQuestion })
+    } catch (error) {
+      res.status(422).json({ error: 'Unprocessable entity' })
+    }
   })
 }
