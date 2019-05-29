8399a62e1b47e3c6ab59b36367f2457edb01306f
Jack Keating <keatjm@gmail.com>
2019-05-29 11:21:17 -0700

    Duplicate the explanatory note about our plan to re-implement moderation/filtering

--- router/api/moderate.js
+++ router/api/moderate.js
@@ -1,5 +1,6 @@
 module.exports = router => {
   router.route('/moderate/message').post((req, res) => {
     // Removed ModerationCtrl's moderateMessage() request, response callback
+    // No longer using cleanspeak, and will be implementing our own moderation logic
   })
 }
