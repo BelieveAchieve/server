ac2fa83e9d65b6b2daa5c30e97f51e0e5072c268
David Nisson <d.m.nisson@gmail.com>
2019-07-01 11:44:36 -0700

    Moved responsibility of updating from helper function to update function

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -1,14 +1,63 @@
 var User = require('../models/User')
 
-// helper to respond to errors
-
-function getProfileIfSuccessful(callback) {
+// helper to check for errors before getting user profile
+function getProfileIfSuccessful (callback) {
   return function (err, user) {
     if (err) {
       return callback(err)
     } else {
       user.getProfile(callback)
     }
+  }
+}
+
+// helper to iterate through keys to be added to an update object
+function iterateKeys (update, data, callback) {
+  var hasUpdate = false
+
+  ;[
+    'firstname',
+    'lastname',
+    'nickname',
+    'picture',
+    'birthdate',
+    'serviceInterests',
+    'gender',
+    'race',
+    'groupIdentification',
+    'computerAccess',
+    'preferredTimes',
+    'phone',
+    'highschool',
+    'currentGrade',
+    'expectedGraduation',
+    'difficultAcademicSubject',
+    'difficultCollegeProcess',
+    'highestLevelEducation',
+    'hasGuidanceCounselor',
+    'gpa',
+    'college',
+    'collegeApplicationsText',
+    'commonCollegeDocs',
+    'academicInterestsText',
+    'testScoresText',
+    'advancedCoursesText',
+    'favoriteAcademicSubject',
+    'extracurricularActivitesText',
+    'referred',
+    'heardFrom',
+    'phonePretty'
+  ].forEach(function (key) {
+    if (data[key]) {
+      update[key] = data[key]
+      hasUpdate = true
+    }
+  })
+
+  if (!hasUpdate) {
+    callback('No fields defined to update')
+  } else {
+    callback(null, update)
   }
 }
 
@@ -30,84 +79,37 @@ module.exports = {
 
     var update = {}
 
-    var hasUpdate = false
-
-    // Define and iterate through keys to add to update object
-    var iterateKeysAndUpdate = function () {
-      ;[
-        'firstname',
-        'lastname',
-        'nickname',
-        'picture',
-        'birthdate',
-        'serviceInterests',
-        'gender',
-        'race',
-        'groupIdentification',
-        'computerAccess',
-        'preferredTimes',
-        'phone',
-        'highschool',
-        'currentGrade',
-        'expectedGraduation',
-        'difficultAcademicSubject',
-        'difficultCollegeProcess',
-        'highestLevelEducation',
-        'hasGuidanceCounselor',
-        'gpa',
-        'college',
-        'collegeApplicationsText',
-        'commonCollegeDocs',
-        'academicInterestsText',
-        'testScoresText',
-        'advancedCoursesText',
-        'favoriteAcademicSubject',
-        'extracurricularActivitesText',
-        'referred',
-        'heardFrom',
-        'phonePretty'
-      ].forEach(function (key) {
-        if (data[key]) {
-          update[key] = data[key]
-          hasUpdate = true
-        }
-      })
-
-      if (!hasUpdate) {
-        return callback('No fields defined to update')
-      }
-
-      if (virtualProps.some(function (key) { return data[key] })) {
-        // save the model that was loaded into memory
-        update.save(getProfileIfSuccessful(callback))
-      }
-      else {
-        // update the model (more efficient, but ignores virtual props)
-        User.findByIdAndUpdate(userId, update, { new: true, runValidators: true }, getProfileIfSuccessful(callback))
-      }
-    }
-
     // Keys to virtual properties
     var virtualProps = ['phonePretty']
+
     if (virtualProps.some(function (key) { return data[key] })) {
       // load model object into memory
       User.findById(userId, function (err, user) {
         if (err) {
           callback(err)
-        }
-        else {
+        } else {
           if (!user) {
             update = new User()
-          }
-          else {
+          } else {
             update = user
           }
-          iterateKeysAndUpdate()
+          iterateKeys(update, data, function (err, update) {
+            if (err) {
+              return callback(err)
+            }
+            // save the model that was loaded into memory, processing the virtuals
+            update.save(getProfileIfSuccessful(callback))
+          })
         }
       })
-    }
-    else {
-      iterateKeysAndUpdate()
+    } else {
+      iterateKeys(update, data, function (err, update) {
+        if (err) {
+          return callback('No fields defined to update')
+        }
+        // update the document directly (more efficient, but ignores virtual props)
+        User.findByIdAndUpdate(userId, update, { new: true, runValidators: true }, getProfileIfSuccessful(callback))
+      })
     }
   }
 }
