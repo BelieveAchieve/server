e76209cd7ec0320f1e4a8022a8516d1b0ca50656
Jack Keating <keatjm@gmail.com>
2019-07-08 19:16:47 -0400

    Formatting cleanup

--- services/MailService.js
+++ services/MailService.js
@@ -1,46 +1,46 @@
-var config = require('../config')
+const config = require('../config')
 const sgMail = require('@sendgrid/mail')
-// Utility functions for sendgrid
 
-var sendEmail = function (apiKey, toEmail, fromEmail, templateId, dynamicData, callback) {
+const sendEmail = function (apiKey, toEmail, fromEmail, templateId, dynamicData, callback) {
   sgMail.setApiKey(apiKey)
-  console.log(apiKey)
+
   const msg = {
     to: toEmail,
     from: fromEmail,
     templateId: templateId,
     dynamic_template_data: dynamicData
   }
-  console.log(msg)
-  sgMail.send(msg,callback)
-  console.log('hello')
+
+  sgMail.send(msg, callback)
 }
 
 module.exports = {
   sendVerification: function (options, callback) {
-    var email = options.email
-    var token = options.token
-    var url = 'http://' + config.client.host + '/#/action/verify/' + token
-    console.log(url)
+    const email = options.email
+    const token = options.token
+    const url = 'http://' + config.client.host + '/action/verify/' + token
+
     sendEmail(config.sendgrid.apiKey, email, config.mail.senders.noreply,
       config.sendgrid.verifyTemplate, {
         'userEmail': email,
         'verifyLink': url
       }, callback)
   },
+
   sendContactForm: function (options, callback) {
-    var email = options.email
-    var responseData = options.responseData
-    console.log(responseData)
-    sendEmail(config.sendgrid.apiKey,email,config.mail.senders.noreply, 
+    const email = options.email
+    const responseData = options.responseData
+
+    sendEmail(config.sendgrid.apiKey, email, config.mail.senders.noreply,
       config.sendgrid.contactTemplate, responseData, callback)
   },
+
   sendReset: function (options, callback) {
-    var email = options.email
-    var token = options.token
-    var url = 'http://' + config.client.host + '/#/setpassword/' + token
-    console.log(url)
-    sendEmail(config.sendgrid.apiKey,email,config.mail.senders.noreply, 
+    const email = options.email
+    const token = options.token
+    const url = 'http://' + config.client.host + '/setpassword/' + token
+
+    sendEmail(config.sendgrid.apiKey, email, config.mail.senders.noreply,
       config.sendgrid.resetTemplate, {
         'userEmail': email,
         'resetLink': url
