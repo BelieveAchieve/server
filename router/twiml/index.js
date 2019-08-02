c49f2d0a74505f73d4a1a019f6b0a9d1634ab19b
David Nisson <d.m.nisson@gmail.com>
2019-08-02 16:06:30 -0700

    comment explaining how Twilio voice calls work

--- router/twiml/index.js
+++ router/twiml/index.js
@@ -5,7 +5,14 @@ const VoiceResponse = twilio.twiml.VoiceResponse
 
 const config = require('../../config')
 
-// Endpoints for generating TwiML instructions
+// These routes are called by Twilio to receive TwiML instructions for
+// voice calls. The Twilio API for voice calling requires that a URL be
+// specified to obtain the TwiML code it needs to generate the voice message.
+// In order to put our message content into a voice call we give Twilio
+// a URL pointing to our own server, which contains the message text encoded
+// in it. When the call is answered Twilio sends a request to this
+// URL, and our server responds with TwiML containing the decoded message text
+// and the configured voice for the text-to-speech conversion.
 module.exports = function (app) {
   console.log('TwiML module')
 
