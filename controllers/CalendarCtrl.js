137a85dbd7fda1dbbc3800e7a9b66c978ede8980
David Nisson <d.m.nisson@gmail.com>
2019-08-29 19:28:01 -0700

    refactoring of some nested callbacks to Promises

--- controllers/CalendarCtrl.js
+++ controllers/CalendarCtrl.js
@@ -1,245 +1,230 @@
 var User = require('../models/User')
 
-module.exports = {
-  initAvailability: function (options, callback) {
-    var userid = options.userid
-    var availability = {
-      Sunday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      },
-      Monday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      },
-      Tuesday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      },
-      Wednesday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      },
-      Thursday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      },
-      Friday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      },
-      Saturday: {
-        '12a': false,
-        '1a': false,
-        '2a': false,
-        '3a': false,
-        '4a': false,
-        '5a': false,
-        '6a': false,
-        '7a': false,
-        '8a': false,
-        '9a': false,
-        '10a': false,
-        '11a': false,
-        '12p': false,
-        '1p': false,
-        '2p': false,
-        '3p': false,
-        '4p': false,
-        '5p': false,
-        '6p': false,
-        '7p': false,
-        '8p': false,
-        '9p': false,
-        '10p': false,
-        '11p': false
-      }
+function initAvailability (user, callback) {
+  var availability = {
+    Sunday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
+    },
+    Monday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
+    },
+    Tuesday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
+    },
+    Wednesday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
+    },
+    Thursday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
+    },
+    Friday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
+    },
+    Saturday: {
+      '12a': false,
+      '1a': false,
+      '2a': false,
+      '3a': false,
+      '4a': false,
+      '5a': false,
+      '6a': false,
+      '7a': false,
+      '8a': false,
+      '9a': false,
+      '10a': false,
+      '11a': false,
+      '12p': false,
+      '1p': false,
+      '2p': false,
+      '3p': false,
+      '4p': false,
+      '5p': false,
+      '6p': false,
+      '7p': false,
+      '8p': false,
+      '9p': false,
+      '10p': false,
+      '11p': false
     }
+  }
 
-    User.findOne({ _id: userid }, function (err, user) {
-      if (err) {
-        return callback(err)
-      }
-      if (!user) {
-        return callback(new Error('No account with that id found.'))
-      }
-      user.availability = availability
-      user.hasSchedule = true
-      user.timezone = ''
-      user.save(function (err, user) {
-        if (err) {
-          callback(err, null)
-        } else {
-          callback(null, availability)
-        }
-      })
+  user.availability = availability
+  user.hasSchedule = true
+  user.timezone = ''
+
+  const promise = user.save()
+    .then(user => {
+      return availability
     })
-  },
 
+  if (!callback) {
+    return promise
+  } else {
+    promise
+      .then(availability => callback(null, availability))
+      .catch(err => callback(err))
+  }
+}
+
+module.exports = {
   getAvailability: function (options, callback) {
     var userid = options.userid
 
-    User.findOne({ _id: userid }, (err, user) => {
-      if (err) {
-        return callback(err)
-      }
-      if (!user) {
-        return callback(new Error('No account with that id found.'))
-      }
-
-      // promise to automatically init availability only if necessary
-      const initAvailabilityPromise = new Promise((resolve, reject) => {
-        if (user.hasSchedule) {
-          resolve(user.availability)
-        } else {
-          this.initAvailability({ userid }, (err, availability) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(availability)
-            }
-          })
+    User.findOne({ _id: userid }).exec()
+      .then(user => {
+        if (!user) {
+          throw new Error('No account with that id found.')
         }
-      })
 
-      initAvailabilityPromise.then((availability) => {
+        if (user.hasSchedule) {
+          return user.availability
+        } else {
+          return initAvailability(user)
+        }
+      }).then(availability => {
         callback(null, availability)
-      }).catch((err) => {
+      }).catch(err => {
+        console.log(err)
         callback(err)
       })
-    })
   },
 
   updateAvailability: function (options, callback) {
