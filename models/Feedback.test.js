fe71876ce01bc4878107531bc672afcc1aa83b6d
Stewart <stewartpwallace@gmail.com>
2019-11-01 06:53:46 -0400

    Remove unit tests from models folder

--- models/Feedback.test.js
+++
@@ -1,7 +0,0 @@
-const test = require('ava')
-const Feedback = require('./Feedback')
-
-test('hello world', t => {
-  const feedback = new Feedback({ studentId: 'student1' })
-  t.is(feedback.studentId, 'student1')
-})
