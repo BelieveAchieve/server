0939ffe29cd74b4d15c1740b3d5f2182904e3724
David Nisson <d.m.nisson@gmail.com>
2019-11-14 11:54:41 -0800

    parseProfile is already called in UserCtrl

--- router/api/user.js
+++ router/api/user.js
@@ -67,7 +67,7 @@ module.exports = function (router) {
           res.json({ err: err })
         } else {
           res.json({
-            user: user.parseProfile()
+            user: user
           })
         }
       }
