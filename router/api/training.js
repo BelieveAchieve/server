ba88cd0160e6b70acfddd233238dfd561d2bc629
David Nisson <d.m.nisson@gmail.com>
2019-11-20 19:40:34 -0800

    simplification of training logic

--- router/api/training.js
+++ router/api/training.js
@@ -1,41 +1,34 @@
 var TrainingCtrl = require('../../controllers/TrainingCtrl')
 
 module.exports = function (router) {
-  router.post('/training/questions', function (req, res) {
-    TrainingCtrl.getQuestions({ category: req.body.category }, function (
-      err,
-      questions
-    ) {
-      if (err) {
-        res.json({ err: err })
-      } else {
-        res.json({
-          msg: 'Questions retrieved from database',
-          questions: questions
-        })
-      }
-    })
+  router.post('/training/questions', async function (req, res) {
+    try {
+      const questions = await TrainingCtrl.getQuestions({ category: req.body.category })
+      res.json({
+        msg: 'Questions retrieved from database',
+        questions: questions
+      })
+    } catch (err) {
+      res.json({ err: err })
+    }
   })
-  router.post('/training/score', function (req, res) {
-    TrainingCtrl.getQuizScore(
-      {
+  router.post('/training/score', async function (req, res) {
+    try {
+      const data = await TrainingCtrl.getQuizScore({
         userid: req.body.userid,
         idAnswerMap: req.body.idAnswerMap,
         category: req.body.category
-      },
-      function (err, data) {
-        if (err) {
-          res.json({ err: err })
-        } else {
-          res.json({
-            msg: 'Score calculated and saved',
-            tries: data.tries,
-            passed: data.passed,
-            score: data.score,
-            idCorrectAnswerMap: data.idCorrectAnswerMap
-          })
-        }
-      }
-    )
+      })
+      
+      res.json({
+        msg: 'Score calculated and saved',
+        tries: data.tries,
+        passed: data.passed,
+        score: data.score,
+        idCorrectAnswerMap: data.idCorrectAnswerMap
+      })
+    } catch (err) {
+      res.json({ err: err })
+    }
   })
 }
