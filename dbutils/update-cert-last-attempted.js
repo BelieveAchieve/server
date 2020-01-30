f3e6051e87bab759736fbff6d295be93aaaf4423
Trey Stevens <trey.stevens@upchieve.org>
2020-01-30 14:16:41 -0500

    update docs that don't have lastAttemptedAt + improved readability

--- dbutils/update-cert-last-attempted.js
+++ dbutils/update-cert-last-attempted.js
@@ -1,30 +1,20 @@
 const mongoose = require('mongoose')
 const dbconnect = require('./dbconnect')
 const User = require('../models/User')
-const certKeys = [
-  'algebra',
-  'geometry',
-  'trigonometry',
-  'precalculus',
-  'calculus',
-  'planning',
-  'essays',
-  'applications'
-]
 
 dbconnect(mongoose, function() {
   User.find({ isVolunteer: true })
     .then(listOfUsers => {
-      const pendingUpdatedUsers = []
-
-      listOfUsers.forEach(user => {
-        certKeys.forEach(category => {
-          const hasAttemptedCategory = user.certifications[category].tries > 0
-          if (hasAttemptedCategory) {
-            user.certifications[category]['lastAttemptedAt'] = user.createdAt
+      const pendingUpdatedUsers = listOfUsers.map(user => {
+        // user.toObject() should remove internal/private Mongoose object keys
+        Object.keys(user.toObject().certifications).forEach(certKey => {
+          const cert = user.certifications[certKey]
+          const hasAttemptedCert = cert.tries > 0
+          if (!cert.lastAttemptedAt && hasAttemptedCert) {
+            cert.lastAttemptedAt = user.createdAt
           }
         })
-        pendingUpdatedUsers.push(user.save())
+        return user.save()
       })
       return Promise.all(pendingUpdatedUsers)
     })
