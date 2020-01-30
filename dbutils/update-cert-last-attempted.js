224bb6b0abd34848200723970ed8e74425dd5f7a
Jack Keating <keatjm@gmail.com>
2020-01-30 15:28:23 -0600

    handle invalid certifications

--- dbutils/update-cert-last-attempted.js
+++ dbutils/update-cert-last-attempted.js
@@ -9,12 +9,20 @@ dbconnect(mongoose, function() {
         // user.toObject() should remove internal/private Mongoose object keys
         Object.keys(user.toObject().certifications).forEach(certKey => {
           const cert = user.certifications[certKey]
+
+          // Early exit if certification isn't defined in the User schema
+          // Also, delete this invalid certification data (i.e. biology cert)
+          if (!cert) {
+            user.set(`certifications.${certKey}`, undefined, { strict: false })
+            return
+          }
+
           const hasAttemptedCert = cert.tries > 0
           if (!cert.lastAttemptedAt && hasAttemptedCert) {
             cert.lastAttemptedAt = user.createdAt
           }
         })
-        return user.save()
+        return user.save().catch(e => console.log(e))
       })
       return Promise.all(pendingUpdatedUsers)
     })
