3e64693aa3dcc0a008749aa0b4fc67356c5562cf
Trey Stevens <trey.stevens@upchieve.org>
2020-02-03 13:18:47 -0500

    middleware to update lastActivityAt for user

--- router/api/index.js
+++ router/api/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var passport = require('../auth/passport')
+var User = require('../../models/User.js')
 
 module.exports = function(app, sessionStore) {
   console.log('API module')
@@ -7,6 +8,27 @@ module.exports = function(app, sessionStore) {
   const io = require('./socket-server')(app)
 
   const router = new express.Router()
+
+  app.use(function(req, res, next) {
+    if (req.user) {
+      const { id, lastActivityAt } = req.user
+      const todaysDateInMS = Date.now()
+      const oneDayElapsed = 1000 * 60 * 60 * 24
+      const lastActivityInMS = new Date(lastActivityAt).getTime()
+
+      if (lastActivityInMS + oneDayElapsed <= todaysDateInMS) {
+        const todaysDateFormatted = new Date(todaysDateInMS)
+        User.updateOne({ _id: id }, { lastActivityAt: todaysDateFormatted })
+          .then(() => next())
+          .catch(err => next(err))
+      } else {
+        next()
+      }
+    } else {
+      next()
+    }
+  })
+
   require('./volunteers')(router)
   require('./user')(router)
   require('./verify')(router)
