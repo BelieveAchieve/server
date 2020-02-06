bb792cb59b46cc1197e7108b7ab60d2b438664bb
David Nisson <d.m.nisson@gmail.com>
2020-02-06 11:49:58 -0800

    lint

--- controllers/CalendarCtrl.js
+++ controllers/CalendarCtrl.js
@@ -11,17 +11,19 @@ module.exports = {
 
     // verify that all of the day-of-week and time-of-day properties are defined on the
     // new availability object
-    if (Object.keys(user.availability.toObject())
-      .some((key) => {
-        if (typeof(availability[key]) === "undefined") {
+    if (
+      Object.keys(user.availability.toObject()).some(key => {
+        if (typeof availability[key] === 'undefined') {
           // day-of-week property needs to be defined
           return true
         }
-        
+
         // time-of-day properties also need to be defined
-        return Object.keys(user.availability[key].toObject())
-          .some((key2) => typeof(availability[key][key2]) === "undefined")
-      })) {
+        return Object.keys(user.availability[key].toObject()).some(
+          key2 => typeof availability[key][key2] === 'undefined'
+        )
+      })
+    ) {
       return callback(new Error('Availability object missing required keys'))
     }
 
