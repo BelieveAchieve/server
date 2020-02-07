5a772759bcafbad67d7a1093fd3d577c9e4c5ab8
David Nisson <d.m.nisson@gmail.com>
2020-02-07 09:34:40 -0800

    combine availability and timezone save into one endpoint

--- router/api/calendar.js
+++ router/api/calendar.js
@@ -2,29 +2,17 @@ var CalendarCtrl = require('../../controllers/CalendarCtrl')
 
 module.exports = function(router) {
   router.post('/calendar/save', function(req, res, next) {
-    CalendarCtrl.updateAvailability(
-      { user: req.user, availability: req.body.availability },
-      function(err, avail) {
-        if (err) {
-          next(err)
-        } else {
-          res.json({
-            msg: 'Availability saved'
-          })
-        }
-      }
-    )
-  })
-  router.post('/calendar/tz/save', function(req, res, next) {
-    CalendarCtrl.updateTimezone({ user: req.user, tz: req.body.tz }, function(
-      err,
-      tz
-    ) {
+    CalendarCtrl.updateSchedule({
+      user: req.user,
+      availability: req.body.availability,
+      tz: req.body.tz
+    },
+    function(err) {
       if (err) {
         next(err)
       } else {
         res.json({
-          msg: 'Timezone saved'
+          msg: 'Schedule saved'
         })
       }
     })
