0c7576d4355360dadfa425c0a3fb75e3ab9819d8
Trey Stevens <trey.stevens@upchieve.org>
2020-02-13 10:22:35 -0500

    check for user before destructuring

--- middleware/add-user-action.js
+++ middleware/add-user-action.js
@@ -1,19 +1,21 @@
 const UserActionCtrl = require('../controllers/UserActionCtrl')
 
 function addUserAction(req, res, next) {
-  const { id } = req.user
+  if (req.user) {
+    const { id } = req.user
 
-  if (req.url === '/api/calendar/save') {
-    UserActionCtrl.updatedAvailability(id)
-  }
+    if (req.url === '/api/calendar/save') {
+      UserActionCtrl.updatedAvailability(id)
+    }
 
-  if (req.url === '/api/training/questions') {
-    const { category } = req.body
-    UserActionCtrl.startedQuiz(id, category)
-  }
+    if (req.url === '/api/training/questions') {
+      const { category } = req.body
+      UserActionCtrl.startedQuiz(id, category)
+    }
 
-  if (req.url === '/api/user' && req.method === 'PUT') {
-    UserActionCtrl.updatedProfile(id)
+    if (req.url === '/api/user' && req.method === 'PUT') {
+      UserActionCtrl.updatedProfile(id)
+    }
   }
 
   next()
