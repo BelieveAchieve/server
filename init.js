400c0086818f51ea9a490966b12abcbd8a8b0dec
Tyler Cummings-Bond <tyler.cummings.bond@gmail.com>
2020-03-29 18:50:03 -0400

    seed test high schools

--- init.js
+++ init.js
@@ -1,16 +1,17 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const ejson = require('mongodb-extended-json')
 
 // Configuration
 var config = require('./config')
 
-// Database
+// Load approved seed files from seeds/ into the Database
 mongoose.connect(config.database, { useNewUrlParser: true })
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log('Connected to database')
-  var collection = db.collection('question')
-  var jsonArr = [
+  const questionCollection = db.collection('question')
+  const approvedQuestionSeeds = [
     'geometry',
     'algebra',
     'trigonometry',
@@ -21,16 +22,52 @@ db.once('open', function() {
     'applications'
   ]
   let promises = []
-  for (var i = 0; i < jsonArr.length; i++) {
+  // Load question seeds into database
+  for (var i = 0; i < approvedQuestionSeeds.length; i++) {
     try {
-      var json = require('./seeds/questions/' + jsonArr[i] + '.json')
+      var json = require('./seeds/questions/' +
+        approvedQuestionSeeds[i] +
+        '.json')
       console.log(json)
+      promises.push(questionCollection.insertMany(json))
     } catch (e) {
       console.log(e)
     }
-
-    promises.push(collection.insertMany(json))
   }
+
+  // Load school seeds into database
+  const approvedSchoolSeeds = ['test_high_schools']
+  // Leverage Mongoose schemas because they allow us to insert/update on keyed collections
+  const dbconnect = require('./dbutils/dbconnect')
+  const School = require('./models/School')
+  dbconnect(mongoose, function() {
+    // For each approved file
+    for (var i = 0; i < approvedSchoolSeeds.length; i++) {
+      try {
+        var schoolSeedData = require('./seeds/schools/' +
+          approvedSchoolSeeds[i] +
+          '.json')
+        const deserializedSchoolSeedData = ejson.deserialize(schoolSeedData)
+        // for each item in each approved file, update the record
+        deserializedSchoolSeedData.forEach(school => {
+          School.updateOne(
+            { upchieveId: school.upchieveId },
+            { $set: school },
+            { new: true, upsert: true },
+            (err, raw) => {
+              if (err) {
+                console.log('Error loading approved schools seed data: ' + err)
+              } else {
+                console.log(school)
+              }
+            }
+          )
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  })
 
   Promise.all(promises)
     .then(() => {
