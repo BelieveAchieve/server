60398c03cbb8070cab1adffa1e849eb7140c39c1
Tyler Cummings-Bond <tyler.cummings.bond@gmail.com>
2020-03-29 21:07:33 -0400

    UPdate users in init

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose')
+const ejson = require('mongodb-extended-json')
 
 // Configuration
 var config = require('./config')
@@ -8,29 +9,55 @@ mongoose.connect(config.database, { useNewUrlParser: true })
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
-  console.log('Connected to database')
-  var collection = db.collection('question')
-  var jsonArr = [
-    'geometry',
-    'algebra',
-    'trigonometry',
-    'precalculus',
-    'calculus',
-    'planning',
-    'essays',
-    'applications'
-  ]
   let promises = []
-  for (var i = 0; i < jsonArr.length; i++) {
-    try {
-      var json = require('./seeds/questions/' + jsonArr[i] + '.json')
-      console.log(json)
-    } catch (e) {
-      console.log(e)
+  // Data about the seed data we intend to import / update from this file
+  const seedDataMetadata = [
+    {
+      folder: '',
+      collection: 'users',
+      idField: 'email',
+      files: ['test_users']
+    },
+    {
+      folder: 'questions/',
+      collection: 'question',
+      idField: 'questionText',
+      files: [
+        'geometry',
+        'algebra',
+        'trigonometry',
+        'precalculus',
+        'calculus',
+        'planning',
+        'essays',
+        'applications'
+      ]
     }
-
-    promises.push(collection.insertMany(json))
-  }
+  ]
+  // For each of the above metadata items, replace each record in each file with the value from seed data
+  seedDataMetadata.forEach(seedDataMetadataItem => {
+    const aCollection = db.collection(seedDataMetadataItem.collection)
+    seedDataMetadataItem.files.forEach(file => {
+      const seedData = require('./seeds/' +
+        seedDataMetadataItem.folder +
+        file +
+        '.json')
+      // use Extended JSON to handle formats like "$date" in json files
+      const deserializedSeedData = ejson.deserialize(seedData)
+      deserializedSeedData.forEach(record => {
+        // Build a Unique ID Key for each record to be updated. Start with empty object
+        const idKey = {}
+        // Add a single key/value: key is seedDataMetadataItem.idField
+        idKey[seedDataMetadataItem.idField] =
+          record[seedDataMetadataItem.idField]
+        const replacePromise = aCollection.replaceOne(idKey, record, {
+          upsert: true
+        })
+        promises.push(replacePromise)
+        console.log(record)
+      })
+    })
+  })
 
   Promise.all(promises)
     .then(() => {
