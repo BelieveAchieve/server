092090d7723aefbcf1670b8bfeb9e1d83ec9764e
Trey Stevens <trey.stevens@upchieve.org>
2020-04-01 00:12:59 -0400

    removed utc_mapping and changed casing of Days constant

--- constants.js
+++ constants.js
@@ -1,39 +1,12 @@
 const DAYS = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday'
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday'
 ]
-
-const UTC_TO_HOUR_MAPPING = {
-  0: '12a',
-  1: '1a',
-  2: '2a',
-  3: '3a',
-  4: '4a',
-  5: '5a',
-  6: '6a',
-  7: '7a',
-  8: '8a',
-  9: '9a',
-  10: '10a',
-  11: '11a',
-  12: '12p',
-  13: '1p',
-  14: '2p',
-  15: '3p',
-  16: '4p',
-  17: '5p',
-  18: '6p',
-  19: '7p',
-  20: '8p',
-  21: '9p',
-  22: '10p',
-  23: '11p'
-}
 
 const USER_ACTION = {
   TYPE: {
@@ -62,6 +35,5 @@ const USER_ACTION = {
 
 module.exports = {
   DAYS,
-  UTC_TO_HOUR_MAPPING,
   USER_ACTION
 }
