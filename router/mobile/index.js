31d63add53678500d758a1f4cdfc2650750442ab
austinhallock <austinhallock@gmail.com>
2020-04-03 22:08:14 -0500

    this branch doesn't need to make it to prod. easier to change here than rebuild ios app tonight

--- router/mobile/index.js
+++ router/mobile/index.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_PRIVATE_KEY_JSON))
   });
 
-  app.use('/setcookie', function(req, res, next) {
+  app.use('/setCookie', function(req, res, next) {
     res.cookie('mobile_cookie', '1', {maxAge: 3600 * 24 * 365 * 10});
     res.redirect(302, 'http://localhost:12380?redirected');
   })
