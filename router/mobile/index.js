8ee5230f3395f84128085aa49d6a0fb011f3c72d
Trey Stevens <trey.stevens@upchieve.org>
2020-04-10 18:51:03 -0400

    added comment to setcookie middleware

--- router/mobile/index.js
+++ router/mobile/index.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
     )
   })
 
+  // used in native app to workaround iOS 3rd party cookie limitation
   app.use('/setcookie', function(req, res, next) {
     res.cookie('mobile_cookie', '1', { maxAge: 3600 * 24 * 365 * 10 })
     res.redirect(302, 'http://localhost:12380?redirected')
