e59ff80f46cfcfa67fccf1c7c093b0d3b1012b36
Jack Keating <keatjm@gmail.com>
2020-04-15 14:48:04 -0500

    deal with ipv4->ipv6 subnet prefix

--- middleware/block-blacklisted-ips.js
+++ middleware/block-blacklisted-ips.js
@@ -4,7 +4,14 @@ function blockBlacklistedIps(req, res, next) {
   const ipBlacklist = config.ipBlacklist
   const userIp = req.ip
 
-  const isBlacklisted = ipBlacklist.indexOf(userIp) !== -1
+  // Remove subnet prefix if present
+  const noPrefixUserIp =
+    userIp.indexOf('::ffff:') === 0 ? userIp.slice(7) : userIp
+
+  // Check blacklist for user's IP with and without subnet prefix
+  const isBlacklisted =
+    ipBlacklist.indexOf(userIp) !== -1 ||
+    ipBlacklist.indexOf(noPrefixUserIp) !== -1
 
   if (isBlacklisted) {
     return res.status(403).json({
