f536c82d7a9c0b0da438adeacac2679c9906b740
Jack Keating <keatjm@gmail.com>
2020-04-20 14:53:55 -0500

    updateMany

--- dbutils/migrate-action-type-profile.js
+++ dbutils/migrate-action-type-profile.js
@@ -9,7 +9,7 @@ dbconnect(mongoose, function() {
     user: { $exists: false }
   })
     .then(() => {
-      return UserAction.update({ actionType: 'PROFILE' }, { actionType: 'ACCOUNT' })
+      return UserAction.updateMany({ actionType: 'PROFILE' }, { actionType: 'ACCOUNT' })
     })
     .catch(err => {
       if (err) {
