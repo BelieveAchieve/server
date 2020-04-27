0b2b80721c69cc6c5eec4a20e45f60435638b740
Eric Liu <eyliubit@gmail.com>
2020-04-27 18:57:35 -0400

    make main api typescript

--- worker/jobs/index.ts
+++ worker/jobs/index.ts
@@ -1,6 +1,6 @@
 import { ProcessPromiseFunction, Queue } from 'bull';
-import { log } from '../logger';
 import { map } from 'lodash';
+import { log } from '../logger';
 import updateElapsedAvailability from './updateElapsedAvailability';
 
 export enum Jobs {
