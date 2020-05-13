4cbc6c1555f02817938744c8ec3d5bdc660b2745
Trey Stevens <trey.stevens@upchieve.org>
2020-05-13 19:52:08 -0400

    update user with data sent from client and send status back

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -1,42 +1,5 @@
 const User = require('../models/User')
-const Volunteer = require('../models/Volunteer')
 const Session = require('../models/Session')
-
-// helper to check for errors before getting user profile
-function getProfileIfSuccessful(callback) {
-  return function(err, user) {
-    if (err) {
-      return callback(err)
-    } else {
-      user.getProfile(callback)
-    }
-  }
-}
-
-// helper to iterate through keys to be added to an update object
-function iterateKeys(update, data, callback) {
-  let hasUpdate = false
-
-  ;[
-    'firstname',
-    'lastname',
-    'phone',
-    'college',
-    'favoriteAcademicSubject',
-    'phonePretty'
-  ].forEach(function(key) {
-    if (data[key]) {
-      update[key] = data[key]
-      hasUpdate = true
-    }
-  })
-
-  if (!hasUpdate) {
-    callback(new Error('No fields defined to update'))
-  } else {
-    callback(null, update)
-  }
-}
 
 module.exports = {
   getVolunteerStats: async user => {
@@ -75,54 +38,6 @@ module.exports = {
     }
 
     return stats
-  },
-
-  update: function(options, callback) {
-    const userId = options.userId
-    const data = options.data || {}
-    let update = {}
-    // Keys to virtual properties
-    const virtualProps = ['phonePretty']
-
-    if (
-      virtualProps.some(function(key) {
-        return data[key]
-      })
-    ) {
-      // load model object into memory
-      // @note: Currently only a volunteer is able to update their profile
-      Volunteer.findById(userId, function(err, user) {
-        if (err) {
-          callback(err)
-        } else {
-          if (!user) {
-            update = new Volunteer()
-          } else {
-            update = user
-          }
-          iterateKeys(update, data, function(err, update) {
-            if (err) {
-              return callback(err)
-            }
-            // save the model that was loaded into memory, processing the virtuals
-            update.save(getProfileIfSuccessful(callback))
-          })
-        }
-      })
-    } else {
-      iterateKeys(update, data, function(err, update) {
-        if (err) {
-          return callback(new Error('No fields defined to update'))
-        }
-        // update the document directly (more efficient, but ignores virtual props)
-        Volunteer.findByIdAndUpdate(
-          userId,
-          update,
-          { new: true, runValidators: true },
-          getProfileIfSuccessful(callback)
-        )
-      })
-    }
   },
 
   deleteUserByEmail: function(userEmail) {
