a74203bd487fc380d3df2ed7f76ef944a2531a6c
Trey Stevens <trey.stevens@upchieve.org>
2020-05-24 11:06:48 -0400

    add module exports

--- utils/map-multi-word-subtopic.ts
+++ utils/map-multi-word-subtopic.ts
@@ -2,11 +2,12 @@ import { INTEGRATED_MATH_MAPPING, PHYSICS_MAPPING } from '../constants';
 import isIntegratedMath from './is-integrated-math';
 import isPhysics from './is-physics';
 
-const mapMultiWordSubtopics = (subtopic): string => {
+const mapMultiWordSubtopic = (subtopic): string => {
   if (isIntegratedMath(subtopic)) return INTEGRATED_MATH_MAPPING[subtopic];
   if (isPhysics(subtopic)) return PHYSICS_MAPPING[subtopic];
 
   return subtopic;
 };
 
-export default mapMultiWordSubtopics;
+module.exports = mapMultiWordSubtopic;
+export default mapMultiWordSubtopic;
