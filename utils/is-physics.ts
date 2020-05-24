124892cfbaee5bbecfbf936789866780037e55ac
Trey Stevens <trey.stevens@upchieve.org>
2020-05-24 11:38:52 -0400

    subTopic -> subtopic

--- utils/is-physics.ts
+++ utils/is-physics.ts
@@ -1,7 +1,7 @@
 import { PHYSICS_MAPPING } from '../constants';
 
-const isPhysics = (subTopic): boolean =>
-  Object.keys(PHYSICS_MAPPING).includes(subTopic);
+const isPhysics = (subtopic): boolean =>
+  Object.keys(PHYSICS_MAPPING).includes(subtopic);
 
 module.exports = isPhysics;
 export default isPhysics;
