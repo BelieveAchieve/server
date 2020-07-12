bf29c9b2560a5439a96cef11ea00899ff98162f5
Trey Stevens <trey.stevens@upchieve.org>
2020-07-12 15:10:08 -0400

    added user interface and enum for availability obj

--- tests/utils/types.ts
+++ tests/utils/types.ts
@@ -1,7 +1,6 @@
 import { Types } from 'mongoose';
 
-// @todo: clean up - use the Student interface from Student.ts when available
-export interface Student {
+export interface User {
   _id: Types.ObjectId;
   email: string;
   // optional use for building registration form
@@ -9,12 +8,16 @@ export interface Student {
   lastName?: string;
   firstname: string;
   lastname: string;
-  highSchoolId: string;
   password: string;
-  zipCode: string;
-  studentPartnerOrg: string;
   referredByCode: Types.ObjectId | string;
   referralCode: string;
+}
+
+// @todo: clean up - use the Student interface from Student.ts when available
+export interface Student extends User {
+  highSchoolId: string;
+  zipCode: string;
+  studentPartnerOrg: string;
 }
 
 // @todo: clean up - use the Reference interface from Volunteer.ts when available
@@ -35,22 +38,12 @@ export interface Reference {
 }
 
 // @todo: clean up - use the Volunteer interface from Volunteer.ts when available
-export interface Volunteer {
-  _id: Types.ObjectId;
-  email: string;
-  // optional use for building registration form
-  firstName?: string;
-  lastName?: string;
-  firstname: string;
-  lastname: string;
-  password: string;
+export interface Volunteer extends User {
   zipCode: string;
   college: string;
   volunteerPartnerOrg?: string;
   favoriteAcademicSubject: string;
-  referredByCode: string;
   phone: string;
-  referralCode: string;
   references?: Array<Reference>;
   photoIdS3Key?: string;
   photoIdStatus?: string;
@@ -76,3 +69,48 @@ export interface StudentRegistrationForm extends Student {
 export interface VolunteerRegistrationForm extends Volunteer {
   terms: boolean;
 }
+
+export enum DAYS {
+  SUNDAY = 'Sunday',
+  MONDAY = 'Monday',
+  TUESDAY = 'Tuesday',
+  WEDNESDAY = 'Wednesday',
+  THURSDAY = 'Thursday',
+  FRIDAY = 'Friday',
+  SATURDAY = 'Saturday'
+}
+
+export enum HOURS {
+  '12AM' = '12a',
+  '1AM' = '1a',
+  '2AM' = '2a',
+  '3AM' = '3a',
+  '4AM' = '4a',
+  '5AM' = '5a',
+  '6AM' = '6a',
+  '7AM' = '7a',
+  '8AM' = '8a',
+  '9AM' = '9a',
+  '10AM' = '10a',
+  '11AM' = '11a',
+  '12PM' = '12p',
+  '1PM' = '1p',
+  '2PM' = '2p',
+  '3PM' = '3p',
+  '4PM' = '4p',
+  '5PM' = '5p',
+  '6PM' = '6p',
+  '7PM' = '7p',
+  '8PM' = '8p',
+  '9PM' = '9p',
+  '10PM' = '10p',
+  '11PM' = '11p'
+}
+
+export type AvailabilityDay = {
+  [hour in HOURS]: boolean;
+};
+
+export type Availability = {
+  [day in DAYS]: AvailabilityDay;
+};
