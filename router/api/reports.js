89d7c4460d74d9dc3179f5ba7239e92487e73478
Trey Stevens <trey.stevens@upchieve.org>
2020-07-14 13:48:09 -0400

    report api route

--- router/api/reports.js
+++ router/api/reports.js
@@ -6,12 +6,23 @@ const ReportService = require('../../services/ReportService');
 module.exports = function(router) {
   router.get('/reports/session-report', passport.isAdmin, async function(
     req,
-    res,
-    next
+    res
   ) {
     try {
-      const sessions = await ReportService.getSessionReport
+      const sessions = await ReportService.sessionReport(req.query)
       res.json({ sessions })
+    } catch (error) {
+      res.sendStatus(500)
+    }
+  });
+
+  router.get('/reports/usage-report', passport.isAdmin, async function(
+    req,
+    res
+  ) {
+    try {
+      const students = await ReportService.usageReport(req.query)
+      res.json({ students })
     } catch (error) {
       res.sendStatus(500)
     }
