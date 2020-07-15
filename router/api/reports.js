9e8e787df29161e3397e84204263a6b28e837971
Trey Stevens <trey.stevens@upchieve.org>
2020-07-14 21:35:02 -0400

    pass errors to error middleware

--- router/api/reports.js
+++ router/api/reports.js
@@ -4,25 +4,27 @@ const ReportService = require('../../services/ReportService')
 module.exports = function(router) {
   router.get('/reports/session-report', passport.isAdmin, async function(
     req,
-    res
+    res,
+    next
   ) {
     try {
       const sessions = await ReportService.sessionReport(req.query)
       res.json({ sessions })
     } catch (error) {
-      res.sendStatus(500)
+      next(error)
     }
   })
 
   router.get('/reports/usage-report', passport.isAdmin, async function(
     req,
-    res
+    res,
+    next
   ) {
     try {
       const students = await ReportService.usageReport(req.query)
       res.json({ students })
     } catch (error) {
-      res.sendStatus(500)
+      next(error)
     }
   })
 }
