616abf8653902952c41b1a401f7b25b6912d8f98
austinhallock <austinhallock@gmail.com>
2020-07-16 16:58:46 -0500

    simplify lint script

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'handle-callback-err': ['error', '^(err|error)$'],
     'no-debugger': 'warn'
   },
-  ignorePatterns: ['dbutils/*', 'utils/zwibblerDecoder.ts'],
+  ignorePatterns: ['dbutils/*', 'utils/zwibblerDecoder.ts', 'mongo-volume/*'],
   overrides: [
     {
       files: ['*.ts'],
