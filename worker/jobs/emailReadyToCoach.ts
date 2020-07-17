0a31189be50e6a585b26f17b55ed782fb3670ccd
Jack Keating <jack.keating@upchieve.org>
2020-07-17 12:06:26 -0500

    fix

--- worker/jobs/emailReadyToCoach.ts
+++ worker/jobs/emailReadyToCoach.ts
@@ -1,6 +1,6 @@
 import VolunteerModel from '../../models/Volunteer';
 import { Volunteer } from '../../models/types';
-import MailService from '../../services/SessionService';
+import MailService from '../../services/MailService';
 import dbconnect from '../../dbutils/dbconnect';
 import { log } from '../logger';
 
@@ -17,6 +17,15 @@ export default async (): Promise<void> => {
   for (const volunteer of volunteers) {
     await MailService.sendReadyToCoachEmail(volunteer);
   }
+
+  await VolunteerModel.updateMany(
+    {
+      isOnboarded: true,
+      isApproved: true,
+      sentReadyToCoachEmail: false
+    },
+    { sentReadyToCoachEmail: true }
+  )
 
   log(`sent ready-to-coach email to ${volunteers.length} volunteers`);
 };
