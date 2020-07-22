e5a673968dcea0d2bbf900e99afddd517e0a4d32
Trey Stevens <trey.stevens@upchieve.org>
2020-07-22 14:25:58 -0400

    hard code custom fields and add partner org

--- services/MailService.js
+++ services/MailService.js
@@ -14,22 +14,18 @@ const options = {
 const putContact = data =>
   axios.put('https://api.sendgrid.com/v3/marketing/contacts', data, options)
 
-const getCustomFields = async () => {
-  const response = await axios.get(
-    'https://api.sendgrid.com/v3/marketing/field_definitions',
-    options
-  )
-
-  const {
-    data: { custom_fields: sendGridCustomFields }
-  } = response
-
-  const customFields = {}
-  for (const field of sendGridCustomFields) {
-    customFields[field.name] = field.id
-  }
-
-  return customFields
+const SG_CUSTOM_FIELDS = {
+  isBanned: 'e3_T',
+  isTestUser: 'e4_T',
+  isVolunteer: 'e6_T',
+  isAdmin: 'e7_T',
+  isFakeUser: 'e8_T',
+  isDeactivated: 'e9_T',
+  joined: 'e10_D',
+  studentPartnerOrg: 'e11_T',
+  studentPartnerOrgDisplay: 'e12_T',
+  volunteerPartnerOrg: 'e13_T',
+  volunteerPartnerOrgDisplay: 'e14_T'
 }
 
 const sendEmail = (
@@ -169,11 +165,32 @@ module.exports = {
   },
 
   createContact: async user => {
-    const customFields = await getCustomFields()
+    const customFields = {
+      [SG_CUSTOM_FIELDS.isBanned]: String(user.isBanned),
+      [SG_CUSTOM_FIELDS.isTestUser]: String(user.isTestUser),
+      [SG_CUSTOM_FIELDS.isVolunteer]: String(user.isVolunteer),
+      [SG_CUSTOM_FIELDS.isAdmin]: String(user.isAdmin),
+      [SG_CUSTOM_FIELDS.isFakeUser]: String(user.isFakeUser),
+      [SG_CUSTOM_FIELDS.isDeactivated]: String(user.isDeactivated),
+      [SG_CUSTOM_FIELDS.joined]: user.createdAt
+    }
 
     const contactListId = user.isVolunteer
       ? config.sendgrid.contactList.volunteers
       : config.sendgrid.contactList.students
+
+    if (user.volunteerPartnerOrg) {
+      customFields[SG_CUSTOM_FIELDS.volunteerPartnerOrg] =
+        user.volunteerPartnerOrg
+      customFields[SG_CUSTOM_FIELDS.volunteerPartnerOrgDisplay] =
+        config.volunteerPartnerManifests[user.volunteerPartnerOrg].name
+    }
+
+    if (user.studentPartnerOrg) {
+      customFields[SG_CUSTOM_FIELDS.studentPartnerOrg] = user.studentPartnerOrg
+      customFields[SG_CUSTOM_FIELDS.studentPartnerOrgDisplay] =
+        config.studentPartnerManifests[user.studentPartnerOrg].name
+    }
 
     const data = {
       list_ids: [contactListId],
@@ -182,15 +199,7 @@ module.exports = {
           first_name: user.firstname,
           last_name: user.lastname,
           email: user.email,
-          custom_fields: {
-            [customFields.isBanned]: String(user.isBanned),
-            [customFields.isTestUser]: String(user.isTestUser),
-            [customFields.isVolunteer]: String(user.isVolunteer),
-            [customFields.isAdmin]: String(user.isAdmin),
-            [customFields.isFakeUser]: String(user.isFakeUser),
-            [customFields.isDeactivated]: String(user.isDeactivated),
-            [customFields.joined]: user.createdAt
-          }
+          custom_fields: customFields
         }
       ]
     }
