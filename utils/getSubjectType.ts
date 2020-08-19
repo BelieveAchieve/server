cfa45682be574d3245a14005855d8609ba8dc048
Trey Stevens <trey.stevens@upchieve.org>
2020-08-18 23:33:15 -0400

    added subject types constants

--- utils/getSubjectType.ts
+++ utils/getSubjectType.ts
@@ -1,23 +1,22 @@
 import {
-  MATH_CERTS,
-  SCIENCE_CERTS,
-  COLLEGE_CERTS,
-  SAT_CERTS,
-  REQUIRED_TRAINING,
+  MATH_SUBJECTS,
+  SCIENCE_SUBJECTS,
+  COLLEGE_SUBJECTS,
+  SAT_SUBJECTS,
+  TRAINING,
   SUBJECT_TYPES
 } from '../constants';
 
 const getSubjectType = (subject): string => {
   let type = '';
 
-  if (Object.values(MATH_CERTS).includes(subject)) type = SUBJECT_TYPES.MATH;
-  if (Object.values(SCIENCE_CERTS).includes(subject))
+  if (Object.values(MATH_SUBJECTS).includes(subject)) type = SUBJECT_TYPES.MATH;
+  if (Object.values(SCIENCE_SUBJECTS).includes(subject))
     type = SUBJECT_TYPES.SCIENCE;
-  if (Object.values(COLLEGE_CERTS).includes(subject))
+  if (Object.values(COLLEGE_SUBJECTS).includes(subject))
     type = SUBJECT_TYPES.COLLEGE;
-  if (Object.values(SAT_CERTS).includes(subject)) type = SUBJECT_TYPES.SAT;
-  if (Object.values(REQUIRED_TRAINING).includes(subject))
-    type = SUBJECT_TYPES.REQUIRED_TRAINING;
+  if (Object.values(SAT_SUBJECTS).includes(subject)) type = SUBJECT_TYPES.SAT;
+  if (Object.values(TRAINING).includes(subject)) type = SUBJECT_TYPES.TRAINING;
 
   return type;
 };
