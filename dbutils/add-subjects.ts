eb1f60a7981f320da4bdaa7e4e54832c66efb0af
Trey Stevens <trey.stevens@upchieve.org>
2020-08-19 17:33:05 -0400

    update subjects migration to use unlockables

--- dbutils/add-subjects.ts
+++ dbutils/add-subjects.ts
@@ -1,22 +1,9 @@
 import mongoose from 'mongoose';
 import dbconnect from './dbconnect';
 import Volunteer from '../models/Volunteer';
-
-const getCertifiedSubjects = (certifications): string[] => {
-  let subjects = [];
-
-  for (const subject in certifications) {
-    if (
-      certifications.hasOwnProperty(subject) &&
-      certifications[subject].passed
-    ) {
-      subjects.push(subject)
-      
-    }
-  }
-
-  return subjects;
-};
+import UserAction from '../models/UserAction';
+import TrainingCtrl from '../controllers/TrainingCtrl';
+import { TRAINING, SUBJECTS } from '../constants';
 
 const upgrade = async (): Promise<void> => {
   try {
@@ -27,20 +14,52 @@ const upgrade = async (): Promise<void> => {
       .lean()
       .exec();
 
-    const pendingUpdates = []
+    const pendingUpdates = [];
 
     for (const volunteer of volunteers) {
-      const subjects = getCertifiedSubjects(volunteer.certifications)
+      // Ignore required training for existing users in order to get unlocked subjects
+      Object.assign(volunteer.certifications, {
+        [TRAINING.TUTORING_SKILLS]: { passed: true, tries: 1 },
+        [TRAINING.UPCHIEVE_101]: { passed: false, tries: 1 },
+        [TRAINING.SAT_STRATEGIES]: { passed: false, tries: 1 },
+        [TRAINING.COLLEGE_COUNSELING]: { passed: false, tries: 1 }
+      });
+      const subjects = TrainingCtrl.getUnlockedSubjects(
+        TRAINING.UPCHIEVE_101,
+        volunteer.certifications
+      );
 
-      pendingUpdates.push(Volunteer.updateOne(
-        { _id: volunteer._id}, 
-        { subjects }, 
-        { runValidators: true } ))
+      // passed and application will be removed from a Volunteer's certifications - use the passed quiz user action
+      // to determine if a volunteer was certified in planning or applications
+      const [passedPlanning, passedApplications] = await Promise.all([
+        UserAction.findOne({
+          action: 'PASSED QUIZ',
+          quizSubcategory: 'PLANNING'
+        })
+          .lean()
+          .exec(),
+        UserAction.findOne({
+          action: 'PASSED QUIZ',
+          quizSubcategory: 'APPLICATIONS'
+        })
+          .lean()
+          .exec()
+      ]);
+
+      if (passedPlanning) subjects.push(SUBJECTS.PLANNING);
+      if (passedApplications) subjects.push(SUBJECTS.APPLICATIONS);
+
+      pendingUpdates.push(
+        Volunteer.updateOne(
+          { _id: volunteer._id },
+          { subjects },
+          { runValidators: true }
+        )
+      );
     }
 
-
-    const result = await Promise.all(pendingUpdates)
-    console.log("The result: ", result)
+    const result = await Promise.all(pendingUpdates);
+    console.log(result);
   } catch (error) {
     console.error(error);
   }
@@ -68,7 +87,7 @@ async function downgrade(): Promise<void> {
 }
 
 // To downgrade the migration run:
-// DOWNGRADE=true npx ts-node dbutils/add-is-onboarded.ts
+// DOWNGRADE=true npx ts-node dbutils/add-subjects.ts
 if (process.env.DOWNGRADE) {
   downgrade();
 } else {
