25e9a12f196b3d67a9c297c740577b5fab1e49ff
Trey Stevens <trey.stevens@upchieve.org>
2020-08-25 15:49:39 -0400

    remove check for passed quiz user action

--- dbutils/add-subjects.ts
+++ dbutils/add-subjects.ts
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 import dbconnect from './dbconnect';
 import Volunteer from '../models/Volunteer';
-import UserAction from '../models/UserAction';
 import TrainingCtrl from '../controllers/TrainingCtrl';
-import { TRAINING, SUBJECTS } from '../constants';
+import { TRAINING } from '../constants';
 
 const upgrade = async (): Promise<void> => {
   try {
@@ -22,32 +21,14 @@ const upgrade = async (): Promise<void> => {
         [TRAINING.TUTORING_SKILLS]: { passed: true, tries: 1 },
         [TRAINING.UPCHIEVE_101]: { passed: false, tries: 1 },
         [TRAINING.SAT_STRATEGIES]: { passed: false, tries: 1 },
+        // @note set to true in getUnlockedSubjects, but we do not want to set this to true
+        // when college counseling course is added since this will unlock college planning and applications
         [TRAINING.COLLEGE_COUNSELING]: { passed: false, tries: 1 }
       });
       const subjects = TrainingCtrl.getUnlockedSubjects(
         TRAINING.UPCHIEVE_101,
         volunteer.certifications
       );
-
-      // passed and application will be removed from a Volunteer's certifications - use the passed quiz user action
-      // to determine if a volunteer was certified in planning or applications
-      const [passedPlanning, passedApplications] = await Promise.all([
-        UserAction.findOne({
-          action: 'PASSED QUIZ',
-          quizSubcategory: 'PLANNING'
-        })
-          .lean()
-          .exec(),
-        UserAction.findOne({
-          action: 'PASSED QUIZ',
-          quizSubcategory: 'APPLICATIONS'
-        })
-          .lean()
-          .exec()
-      ]);
-
-      if (passedPlanning) subjects.push(SUBJECTS.PLANNING);
-      if (passedApplications) subjects.push(SUBJECTS.APPLICATIONS);
 
       pendingUpdates.push(
         Volunteer.updateOne(
