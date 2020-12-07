d96611474c17f24a53676e296bccfa37454c7323
Trey Stevens <trey.stevens@upchieve.org>
2020-12-07 18:43:03 -0500

    calculateElapsedAvailability test update

--- tests/controllers/UserCtrl.test.ts
+++ tests/controllers/UserCtrl.test.ts
@@ -1,320 +1,130 @@
-const Volunteer = require('../../models/Volunteer')
-const UserCtrl = require('../../controllers/UserCtrl')
-const {
+import { buildVolunteer } from '../generate';
+import UserCtrl from '../../controllers/UserCtrl';
+import {
   flexibleHoursSelected,
   noHoursSelected,
   allHoursSelected
-} = require('../mocks/volunteer-availability')
+} from '../mocks/volunteer-availability';
 
-const goodUser = new Volunteer({
-  email: 'email@email.com',
-  password: 'password',
+describe('calculateElapsedAvailability', () => {
+  test('Should not calculate elapsed availability for volunteers who are not onboarded', () => {
+    // EST Time Zone for dates
+    const lastModifiedDate = '2020-02-06T00:52:59.538-05:00';
+    const newModifiedDate = '2020-02-09T12:40:00.000-05:00';
+    const angelou = buildVolunteer({
+      availability: flexibleHoursSelected,
+      availabilityLastModifiedAt: lastModifiedDate,
+      isApproved: true,
+      isOnboarded: false
+    });
 
-  verified: true,
-  verificationToken: 'verificationToken',
-  passwordResetToken: 'passwordResetToken',
+    const result = UserCtrl.calculateElapsedAvailability(
+      angelou,
+      newModifiedDate
+    );
+    const expectedElapsedAvailability = 0;
+    expect(result).toBe(expectedElapsedAvailability);
+  });
 
-  // Profile data
-  firstname: 'firstname',
-  lastname: 'lastname',
-  phone: 5555555555,
-  college: 'college',
+  test('Should not calculate elapsed availability for volunteers who are not approved', () => {
+    // EST Time Zone for dates
+    const lastModifiedDate = '2020-02-06T00:52:59.538-05:00';
+    const newModifiedDate = '2020-02-09T12:40:00.000-05:00';
+    const plath = buildVolunteer({
+      availability: flexibleHoursSelected,
+      availabilityLastModifiedAt: lastModifiedDate,
+      isApproved: false,
+      isOnboarded: true
+    });
 
-  availability: {
-    Sunday: {
-      '12a': false,
-      '1a': false,
-      '2a': false,
-      '3a': false,
-      '4a': false,
-      '5a': false,
-      '6a': false,
-      '7a': false,
-      '8a': false,
-      '9a': false,
-      '10a': false,
-      '11a': false,
-      '12p': false,
-      '1p': false,
-      '2p': false,
-      '3p': false,
-      '4p': false,
-      '5p': false,
-      '6p': false,
-      '7p': false,
-      '8p': false,
-      '9p': false,
-      '10p': false,
-      '11p': false
-    },
-    Monday: {
-      '12a': false,
-      '1a': false,
-      '2a': false,
-      '3a': false,
-      '4a': false,
-      '5a': false,
-      '6a': false,
-      '7a': false,
-      '8a': false,
-      '9a': false,
-      '10a': false,
-      '11a': false,
-      '12p': false,
-      '1p': false,
-      '2p': false,
-      '3p': false,
-      '4p': false,
-      '5p': false,
-      '6p': false,
-      '7p': false,
-      '8p': false,
-      '9p': false,
-      '10p': false,
-      '11p': false
-    },
-    Tuesday: {
-      '12a': false,
-      '1a': false,
-      '2a': false,
-      '3a': false,
-      '4a': false,
-      '5a': false,
-      '6a': false,
-      '7a': false,
-      '8a': false,
-      '9a': false,
-      '10a': false,
-      '11a': false,
-      '12p': false,
-      '1p': false,
-      '2p': false,
-      '3p': false,
-      '4p': false,
-      '5p': false,
-      '6p': false,
-      '7p': false,
-      '8p': false,
-      '9p': false,
-      '10p': false,
-      '11p': false
-    },
-    Wednesday: {
-      '12a': false,
-      '1a': false,
-      '2a': false,
-      '3a': false,
-      '4a': false,
-      '5a': false,
-      '6a': false,
-      '7a': false,
-      '8a': false,
-      '9a': false,
-      '10a': false,
-      '11a': false,
-      '12p': false,
-      '1p': false,
-      '2p': false,
-      '3p': false,
-      '4p': false,
-      '5p': false,
-      '6p': false,
-      '7p': false,
-      '8p': false,
-      '9p': false,
-      '10p': false,
-      '11p': false
-    },
-    Thursday: {
-      '12a': false,
-      '1a': false,
-      '2a': false,
-      '3a': false,
-      '4a': false,
-      '5a': false,
-      '6a': false,
-      '7a': false,
-      '8a': false,
-      '9a': false,
-      '10a': false,
-      '11a': false,
-      '12p': false,
-      '1p': false,
-      '2p': false,
-      '3p': false,
-      '4p': false,
-      '5p': false,
-      '6p': false,
-      '7p': false,
-      '8p': false,
-      '9p': false,
-      '10p': false,
-      '11p': false
-    },
-    Friday: {
-      '12a': false,
-      '1a': false,
-      '2a': false,
-      '3a': false,
-      '4a': false,
-      '5a': false,
-      '6a': false,
-      '7a': false,
-      '8a': false,
-      '9a': false,
-      '10a': false,
-      '11a': false,
-      '12p': false,
-      '1p': false,
-      '2p': false,
-      '3p': false,
-      '4p': false,
-      '5p': false,
-      '6p': false,
-      '7p': false,
-      '8p': false,
-      '9p': false,
-      '10p': false,
-      '11p': false
-    },
-    Saturday: {
-      '12a': true,
-      '1a': true,
-      '2a': true,
-      '3a': true,
-      '4a': true,
-      '5a': true,
-      '6a': true,
-      '7a': true,
-      '8a': true,
-      '9a': true,
-      '10a': true,
-      '11a': true,
-      '12p': true,
-      '1p': true,
-      '2p': true,
-      '3p': true,
-      '4p': true,
-      '5p': true,
-      '6p': true,
-      '7p': true,
-      '8p': true,
-      '9p': true,
-      '10p': true,
-      '11p': true
-    }
-  },
-  timezone: 'EST',
-  pastSessions: null
-})
+    const result = UserCtrl.calculateElapsedAvailability(
+      plath,
+      newModifiedDate
+    );
+    const expectedElapsedAvailability = 0;
+    expect(result).toBe(expectedElapsedAvailability);
+  });
 
-test('Elapsed availability for partially onboarded users', () => {
-  // EST Time Zone for dates
-  const lastModifiedDate = '2020-02-06T00:52:59.538-05:00'
-  const newModifiedDate = '2020-02-09T12:40:00.000-05:00'
-  const expected = 0
-  goodUser.availability = flexibleHoursSelected
-  goodUser.availabilityLastModifiedAt = lastModifiedDate
-  const result = UserCtrl.calculateElapsedAvailability(
-    goodUser.toObject(),
-    newModifiedDate
-  )
-  expect(expected).toBe(result)
-})
+  test('Elapsed availability over 3 days with no hours available', () => {
+    // EST Time Zone for dates
+    const lastModifiedDate = '2020-02-06T12:52:59.538-05:00';
+    const newModifiedDate = '2020-02-09T13:40:00.000-05:00';
+    const whitman = buildVolunteer({
+      availability: noHoursSelected,
+      availabilityLastModifiedAt: lastModifiedDate,
+      isApproved: true,
+      isOnboarded: true
+    });
 
-test('Elapsed availability over 3 days with no hours available', () => {
-  // EST Time Zone for dates
-  const lastModifiedDate = '2020-02-06T12:52:59.538-05:00'
-  const newModifiedDate = '2020-02-09T13:40:00.000-05:00'
-  const expected = 0
-  goodUser.availability = noHoursSelected
-  goodUser.availabilityLastModifiedAt = lastModifiedDate
-  const result = UserCtrl.calculateElapsedAvailability(
-    goodUser.toObject(),
-    newModifiedDate
-  )
-  expect(expected).toBe(result)
-})
+    const result = UserCtrl.calculateElapsedAvailability(
+      whitman,
+      newModifiedDate
+    );
+    const expectedElapsedAvailability = 0;
+    expect(result).toBe(expectedElapsedAvailability);
+  });
 
-test('Elapsed availability over 3 days with all hours available and 7 hours out of range', () => {
-  // EST Time Zone for dates
-  const lastModifiedDate = '2020-02-06T00:52:59.538-05:00'
-  const newModifiedDate = '2020-02-09T19:40:00.000-05:00'
-  const expected = 90
-  goodUser.availability = allHoursSelected
-  goodUser.availabilityLastModifiedAt = lastModifiedDate
-  // @todo Make Volunteer.test.js with an onboarded and partially onboarded Volunteer
-  // Onboard the user
-  goodUser.isVolunteer = true
-  goodUser.certifications['algebra'].passed = true
-  goodUser.isOnboarded = true
-  const result = UserCtrl.calculateElapsedAvailability(
-    goodUser.toObject(),
-    newModifiedDate
-  )
-  expect(expected).toBe(result)
+  test('Elapsed availability over 3 days with all hours available and 7 hours out of range', () => {
+    // EST Time Zone for dates
+    const lastModifiedDate = '2020-02-06T00:52:59.538-05:00';
+    const newModifiedDate = '2020-02-09T19:40:00.000-05:00';
+    const tennyson = buildVolunteer({
+      availability: allHoursSelected,
+      availabilityLastModifiedAt: lastModifiedDate,
+      isApproved: true,
+      isOnboarded: true
+    });
 
-  // set user back to default
-  goodUser.isVolunteer = false
-  goodUser.certifications['algebra'].passed = false
-  goodUser.isOnboarded = false
-})
+    const result = UserCtrl.calculateElapsedAvailability(
+      tennyson,
+      newModifiedDate
+    );
+    const expectedElapsedAvailability = 90;
+    expect(result).toBe(expectedElapsedAvailability);
+  });
 
-test('Elapsed availability over 3 days with flexible hours available', () => {
-  // EST Time Zone for dates
-  const lastModifiedDate = '2020-02-06T00:52:59.538-05:00'
-  const newModifiedDate = '2020-02-09T12:40:00.000-05:00'
-  const expected = 16
-  goodUser.availability = flexibleHoursSelected
-  goodUser.availabilityLastModifiedAt = lastModifiedDate
-  // @todo Make Volunteer.test.js with an onboarded and partially onboarded Volunteer
-  // Onboard the user
-  goodUser.isVolunteer = true
-  goodUser.certifications['algebra'].passed = true
-  goodUser.isOnboarded = true
-  const result = UserCtrl.calculateElapsedAvailability(
-    goodUser.toObject(),
-    newModifiedDate
-  )
-  expect(expected).toBe(result)
+  test('Elapsed availability over 3 days with flexible hours available', () => {
+    // EST Time Zone for dates
+    const lastModifiedDate = '2020-02-06T00:52:59.538-05:00';
+    const newModifiedDate = '2020-02-09T12:40:00.000-05:00';
+    const emerson = buildVolunteer({
+      availability: flexibleHoursSelected,
+      availabilityLastModifiedAt: lastModifiedDate,
+      isApproved: true,
+      isOnboarded: true
+    });
 
-  // set user back to default
-  goodUser.isVolunteer = false
-  goodUser.certifications['algebra'].passed = false
-  goodUser.isOnboarded = false
-})
+    const result = UserCtrl.calculateElapsedAvailability(
+      emerson,
+      newModifiedDate
+    );
+    const expectedElapsedAvailability = 16;
+    expect(result).toBe(expectedElapsedAvailability);
+  });
 
-/** 
- * flexibleHoursSelected mapped:
- { Sunday: 3,
-  Monday: 6,
-  Tuesday: 6,
-  Wednesday: 5,
-  Thursday: 3,
-  Friday: 6,
-  Saturday: 5 }
-**/
-test('Elapsed availability over 23 days with flexible hours available', () => {
-  // EST Time Zone for dates
-  const lastModifiedDate = '2020-02-02T05:21:39.538-05:00'
-  const newModifiedDate = '2020-02-25T16:20:42.000-05:00'
-  const expected = 114
-  goodUser.availability = flexibleHoursSelected
-  goodUser.availabilityLastModifiedAt = lastModifiedDate
+  /** 
+   * flexibleHoursSelected mapped:
+   { Sunday: 3,
+    Monday: 6,
+    Tuesday: 6,
+    Wednesday: 5,
+    Thursday: 3,
+    Friday: 6,
+    Saturday: 5 }
+  **/
+  test('Elapsed availability over 23 days with flexible hours available', () => {
+    // EST Time Zone for dates
+    const lastModifiedDate = '2020-02-02T05:21:39.538-05:00';
+    const newModifiedDate = '2020-02-25T16:20:42.000-05:00';
+    const poe = buildVolunteer({
+      availability: flexibleHoursSelected,
+      availabilityLastModifiedAt: lastModifiedDate,
+      isApproved: true,
+      isOnboarded: true
+    });
 
-  // @todo Make Volunteer.test.js with an onboarded and partially onboarded Volunteer
-  // Onboard the user
-  goodUser.isVolunteer = true
-  goodUser.certifications['algebra'].passed = true
-  goodUser.isOnboarded = true
-  const result = UserCtrl.calculateElapsedAvailability(
-    goodUser.toObject(),
-    newModifiedDate
-  )
-  expect(expected).toBe(result)
-
-  // set user back to default
-  goodUser.isVolunteer = false
-  goodUser.certifications['algebra'].passed = false
-  goodUser.isOnboarded = false
-})
+    const result = UserCtrl.calculateElapsedAvailability(poe, newModifiedDate);
+    const expectedElapsedAvailability = 114;
+    expect(result).toBe(expectedElapsedAvailability);
+  });
+});
