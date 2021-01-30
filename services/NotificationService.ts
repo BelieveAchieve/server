c862bcaf0aad4f100252bcdb6c2b03cd6d59d1d4
Trey Stevens <treykstevens.tks@gmail.com>
2021-01-29 21:12:51 -0500

    updates to notification model and service

--- services/NotificationService.ts
+++ services/NotificationService.ts
@@ -1,26 +1,59 @@
-const Notification = require('../models/Notification')
-const SessionService = require('../services/SessionService')
+import NotificationModel, { Notification } from '../models/Notification';
+import SessionService from './SessionService';
 
-module.exports = {
-  getSessionNotifications: async sessionId => {
-    const session = await SessionService.getSession(sessionId)
-    return Notification.aggregate([
-      {
-        $match: {
-          $expr: {
-            $in: ['$_id', session.notifications]
-          }
+export const getNotification = (
+  query,
+  projection = {}
+): Promise<Notification> => {
+  return NotificationModel.findOne(query)
+    .select(projection)
+    .lean()
+    .exec();
+};
+
+export const getNotificationWithVolunteer = async (
+  notificationId
+): Promise<Notification> => {
+  const [notification] = await NotificationModel.aggregate([
+    {
+      $match: {
+        _id: notificationId
+      }
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'volunteer',
+        foreignField: '_id',
+        as: 'volunteer'
+      }
+    },
+    { $unwind: '$volunteer' }
+  ]);
+
+  return notification;
+};
+
+export const getSessionNotifications = async (
+  sessionId
+): Promise<Notification[]> => {
+  const session = await SessionService.getSession(sessionId);
+  return NotificationModel.aggregate([
+    {
+      $match: {
+        $expr: {
+          $in: ['$_id', session.notifications]
         }
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'volunteer',
-          foreignField: '_id',
-          as: 'volunteer'
-        }
-      },
-      { $unwind: '$volunteer' }
-    ]).exec()
-  }
-}
+      }
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'volunteer',
+        foreignField: '_id',
+        as: 'volunteer'
+      }
+    },
+    { $unwind: '$volunteer' }
+  ]).exec();
+};
