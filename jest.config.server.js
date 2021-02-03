5afb6f41ea9b3555d4a1e3d9b2d96a6595457e02
Trey Stevens <treykstevens.tks@gmail.com>
2021-02-03 16:59:09 -0500

    add typescript transpile for jest

--- jest.config.server.js
+++ jest.config.server.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: '@shelf/jest-mongodb',
   setupFiles: ['./server/tests/global.ts'],
   watchPathIgnorePatterns: ['globalConfig'],
-  roots: ["./server"]
+  roots: ["./server"],
+  transform: {
+    "^.+\\.ts?$": "<rootDir>/node_modules/ts-jest"
+  },
 }
