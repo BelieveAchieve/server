ec9d33ebdef3e9847caca2ada002bd22d9f410e5
Trey Stevens <treykstevens.tks@gmail.com>
2021-02-10 23:42:11 -0500

    uploadedToStorage and getDocFromStorage

--- services/WhiteboardService.ts
+++ services/WhiteboardService.ts
@@ -1,5 +1,8 @@
 import Session from '../models/Session';
+import config from '../config';
+import logger from '../logger';
 import { redisGet, redisSet, redisDel, redisAppend } from './RedisService';
+import { getBlob, uploadBlob } from './AzureService';
 
 const sessionIdToKey = (id): string => `zwibbler-${id}`;
 
@@ -27,6 +30,7 @@ export const deleteDoc = (sessionId): Promise<void> => {
   return redisDel(sessionIdToKey(sessionId));
 };
 
+// @todo: remove once whiteboard docs are in azure storage
 export const getFinalDocState = async (sessionId): Promise<string> => {
   const { whiteboardDoc } = await Session.findOne({ _id: sessionId })
     .select('whiteboardDoc')
@@ -34,4 +38,48 @@ export const getFinalDocState = async (sessionId): Promise<string> => {
     .exec();
 
   return whiteboardDoc;
+};
+
+export const uploadedToStorage = async (
+  sessionId: string,
+  whiteboardDoc: string,
+  attempts = 0
+): Promise<boolean> => {
+  try {
+    await uploadBlob({
+      containerName: config.whiteboardStorageContainer,
+      blobName: sessionId,
+      content: whiteboardDoc
+    });
+    return true;
+  } catch (error) {
+    if (attempts === 1) {
+      logger.error(
+        `Retry uploading of whiteboard failed ${sessionId}: ${error.message}`
+      );
+
+      return false;
+    }
+
+    logger.error(
+      `Uploading of whiteboard failed ${sessionId}, retrying: ${error.message}`
+    );
+    attempts++;
+    return uploadedToStorage(sessionId, whiteboardDoc, attempts);
+  }
+};
+
+export const getDocFromStorage = async (sessionId: string): Promise<string> => {
+  try {
+    const whiteboardDoc = await getBlob({
+      containerName: config.whiteboardStorageContainer,
+      blobName: sessionId
+    });
+    return whiteboardDoc;
+  } catch (error) {
+    logger.error(
+      `Getting the whiteboard failed ${sessionId}: ${error.message}`
+    );
+    return '';
+  }
 };
